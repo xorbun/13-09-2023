2aa6ee6b14881397d927062434b21ba9dd9f5ec1
Bruno Capobianco <bruno.capobianco@outlook.it>
2023-09-13 16:27:58 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,33 @@
+const newNote=document.getElementById("newReminder");
+
+newNote.addEventListener("submit", function(e)
+{
+    e.preventDefault();
+   const reminderField=document.getElementById("reminder");
+   
+   const newReminder2=
+   {
+    title: reminderField.value,
+   };
+   
+
+   const newCard=document.createElement("div");
+   newCard.classList.add("RememberTo");
+   newCard.innerHTML=`<p>${newReminder2.title}</p> 
+   <button onclick="deletememo(event)">CANCELLA MEMO</button> `
+
+   const section=document.getElementById("savedmemo");
+   section.appendChild(newCard);
+   reminderField.value="";
+   
+})
+const deletememo= function(a)
+   {
+        const clickedbut=a.target;
+        console.log(clickedbut.parentElement);
+        const selector=document.querySelector("rememberto");
+        
+        console.log(selector);
+        const finish=clickedbut.parentElement.remove(); 
+        
+   }
